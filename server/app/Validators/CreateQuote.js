344838544a6e159f65b1d9a479002953435c23b2
Vlad Barabolya <barabolya.vlad@gmail.com>
2021-11-29 14:19:58 +0200

    Quotes valid

--- server/app/Validators/CreateQuote.js
+++ server/app/Validators/CreateQuote.js
@@ -7,7 +7,6 @@ const Base = require('./Base')
 class CreateQuote extends Base {
   static schema = () =>
     yup.object().shape({
-      book_id: yup.number().integer().min(0).required(),
       quote: yup.string().trim().min(1).required()
     })
 }
